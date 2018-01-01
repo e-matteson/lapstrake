83fa9027063485bfea7da0ad75cd0f5621eb38f6
Justin <zallambo@gmail.com>
2018-01-01 14:08:22 -0500

    Refactor main.rs

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,9 @@
 
 
 extern crate csv;
+extern crate serde;
+#[macro_use]
+extern crate serde_derive;
 #[macro_use]
 extern crate failure;
 extern crate nalgebra;
@@ -20,11 +23,10 @@ mod station;
 mod render_3d;
 mod render_2d;
 
+use std::process;
 use std::path::Path;
 use load::read_data;
 use failure::Error;
-
-// use scad_dots::harness::{preview_model};
 
 use unit::Feet;
 use spec::Spec;
@@ -33,7 +35,7 @@ use render_2d::SvgDoc;
 
 
 const STATION_RESOLUTION: usize = 20;
-const PLANK_RESOLUTION: usize = 30;
+const PLANK_RESOLUTION: usize = 1;
 const NUM_PLANKS: usize = 10;
 const OVERLAP: Feet = Feet {
     feet: 0,
@@ -42,35 +44,42 @@ const OVERLAP: Feet = Feet {
 };
 
 fn main() {
-    match read_data(Path::new("data2.csv")) {
-        Ok(data) => {
-            let spec = Spec {
-                config: Config { stuff: 0 },
-                data: data,
-            };
-            let hull = spec.get_hull(STATION_RESOLUTION).unwrap();
+    let data = check_error(read_data(Path::new("data.csv")));
+    let spec = Spec {
+        config: Config { stuff: 0 },
+        data: data,
+    };
+    let hull = check_error(spec.get_hull(STATION_RESOLUTION));
 
-            // Show the half-breadth curves for each station/cross-section
-            let mut doc = SvgDoc::new();
-            doc.append_paths(hull.draw_half_breadths());
-            doc.save("half-breadth.svg").unwrap();
+    // Show the half-breadth curves for each station/cross-section
+    let mut doc = SvgDoc::new();
+    doc.append_paths(hull.draw_half_breadths());
+    check_error(doc.save("half-breadth.svg"));
 
-            // Show all planks overlayed on each other
-            let planks = hull.get_planks(NUM_PLANKS, OVERLAP.into(), PLANK_RESOLUTION)
-                .unwrap();
-            let flattened_planks: Vec<_> = planks
-                .iter()
-                .map(|plank| plank.flatten().unwrap())
-                .collect();
-            let mut doc = SvgDoc::new();
-            for plank in &flattened_planks {
-                doc.append_path(plank.render_2d());
-            }
-            doc.save("plank.svg").unwrap();
+    // Show all planks overlayed on each other
+    let planks = check_error(hull.get_planks(NUM_PLANKS,
+                                             OVERLAP.into(),
+                                             PLANK_RESOLUTION));
+    let flattened_planks: Vec<_> = planks
+        .iter()
+        .map(|plank| check_error(plank.flatten()))
+        .collect();
+    let mut doc = SvgDoc::new();
+    for plank in &flattened_planks {
+        doc.append_path(plank.render_2d());
+    }
+    check_error(doc.save("plank.svg"));
 
-            println!("ok");
+    println!("ok");
+}
+
+fn check_error<T>(result: Result<T, Error>) -> T {
+    match result {
+        Ok(ans)  => ans,
+        Err(err) => {
+            print_error(err);
+            process::exit(1);
         }
-        Err(err) => print_error(err),
     }
 }
 
